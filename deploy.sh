2bfed761303616c10582b08c1b828181ff82b417
ktownag <42381296+ktownag@users.noreply.github.com>
2020-09-21 13:07:08 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+#wget --no-check-certificate -O ibmv2ws.sh https://www.armn1.ml/ibmv2ws.sh && chmod +x ibmv2ws.sh  && ./ibmv2ws.sh
 read -p "请输入应用程序名称:" appname
 read -p "请设置你的容器内存大小(默认256):" ramsize
 if [ -z "$ramsize" ];then
